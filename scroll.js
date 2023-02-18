60ad4b4ab8393f55f602eaba484920dad894405e
TrevorWood98 <100435421+TrevorWood98@users.noreply.github.com>
2023-02-17 18:05:52 -0600

    Delete scroll.js

--- scroll.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function () {
-    setInterval(function () {
-        $('#carousel').animate({
-            scrollLeft: $('#carousel').scrollLeft() + 500
-        }, 1000);
-    }, 3000);
-});
-
